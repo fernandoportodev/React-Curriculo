40ad4b51b5f7820b0a68dc730ae39e36cfdfb0f2
Fernando Porto <fernandogomes143@gmail.com>
2022-06-04 02:25:48 -0300

    @fernandoportodev Realizado alterações no componente Inicio

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -4,7 +4,6 @@ export const Inicio = () => {
     return(
         <div>
         <h1 className="text-center my-5">Seja Bem-vindo ao CMS Currículos</h1>
-        <a href="https://ilegra.com/" target="_blank" className="d-flex justify-content-center"><img src="https://i.imgur.com/CbHubZy.png" alt="Logo Ilegra" /></a>
         </div>
     )
 }
